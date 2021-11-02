ba4b0c5da462ebce2eeac70b83fec09bd4459bac
moradalkhatib <muradalkhateb@icloud.com>
2021-11-02 19:34:38 +0200

    encryptAnddecrypt

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,15 @@
-from caesar_cipher import __version__
+from  caesar_cipher.caesar import encrypt ,decrypt ,crack
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_encrypt():
+    expected = 'Wi xkwo sc Webkn Kvurkdsl'
+    actual = encrypt('My name is Murad Alkhatib', 10)
+    assert actual == expected
+
+def test_decrypt():
+    expected = 'My name is Murad Alkhatib'
+    actual = decrypt('Wi xkwo sc Webkn Kvurkdsl', 10)
+    assert actual == expected 
+
+def test_crack():
+    assert crack('Wi xkwo sc Webkn Kvurkdsl') == 'My name is Murad Alkhatib'
